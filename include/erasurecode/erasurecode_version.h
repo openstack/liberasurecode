0c703b047a851aec93384e1eefc39155816ed445
Tushar Gohad <tushar.gohad@intel.com>
2015-09-06 06:25:41 +0000

    Release 1.0.9

--- include/erasurecode/erasurecode_version.h
+++ include/erasurecode/erasurecode_version.h
@@ -25,9 +25,9 @@
 #ifndef _ERASURECODE_VERSION_H_
 #define _ERASURECODE_VERSION_H_
 
-#define _MAJOR 0
-#define _MINOR 9
-#define _REV 10
+#define _MAJOR 1
+#define _MINOR 0
+#define _REV 9
 #define _VERSION(x, y, z) ((x << 16) | (y << 8) | (z))
 
 #define LIBERASURECODE_VERSION _VERSION(_MAJOR, _MINOR, _REV)
