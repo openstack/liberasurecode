8788462b7e354eb3769607acbe8175d6f0275f7a
Tushar Gohad <tushar.gohad@intel.com>
2015-12-10 04:13:08 -0700

    Fix minor rev

--- include/erasurecode/erasurecode_version.h
+++ include/erasurecode/erasurecode_version.h
@@ -26,8 +26,8 @@
 #define _ERASURECODE_VERSION_H_
 
 #define _MAJOR 1
-#define _MINOR 0
-#define _REV 9
+#define _MINOR 1
+#define _REV 2
 #define _VERSION(x, y, z) ((x << 16) | (y << 8) | (z))
 
 #define LIBERASURECODE_VERSION _VERSION(_MAJOR, _MINOR, _REV)
