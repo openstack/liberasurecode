448c7d0d1cecca219c5d094f0d5e9ccecc7d119c
Thiago da Silva <thiago@redhat.com>
2016-12-07 18:39:37 +0000

    1.4.0 release
    Change-Id: I1d8d6b5711a503eaa7c57c70b4c20a329f572af2
    Signed-off-by: Thiago da Silva <thiago@redhat.com>

--- include/erasurecode/erasurecode_version.h
+++ include/erasurecode/erasurecode_version.h
@@ -26,8 +26,8 @@
 #define _ERASURECODE_VERSION_H_
 
 #define _MAJOR 1
-#define _MINOR 3
-#define _REV 1
+#define _MINOR 4
+#define _REV 0
 #define _VERSION(x, y, z) ((x << 16) | (y << 8) | (z))
 
 #define LIBERASURECODE_VERSION _VERSION(_MAJOR, _MINOR, _REV)
