412e2903a43a2702b67feacbb8bd6b315a28ec26
Tim Burke <tim.burke@gmail.com>
2024-09-13 13:03:20 -0700

    Fix Jerasurecode .so name
    Some distributions (such as Ubuntu, and presumably Debian) provide only
    libJerasure.so.2.0.0 and libJerasure.so.2 (as a symlink to the specific
    version), with no libJerasure.so symlink. On those, some tests would
    previously erroneously skip with
    
       Could not open Jerasure backend.  Install Jerasure or fix
       LD_LIBRARY_PATH.  Passing.
    
    Change-Id: I778543e7c4cfb37d1baf1ee9a1823a809a343343

--- include/erasurecode/alg_sig.h
+++ include/erasurecode/alg_sig.h
@@ -37,7 +37,7 @@ struct jerasure_mult_routines {
 #if defined(__MACOS__) || defined(__MACOSX__) || defined(__OSX__) || defined(__APPLE__)
 #define JERASURE_SONAME "libJerasure.dylib"
 #else
-#define JERASURE_SONAME "libJerasure.so"
+#define JERASURE_SONAME "libJerasure.so.2"
 #endif
 
 typedef struct alg_sig_s
